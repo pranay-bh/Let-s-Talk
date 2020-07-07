d6f38b969d083d57ee5c8dd8f09d8a1e4007acfa
pranay <pranaybhatia80@gmail.com>
2020-07-07 13:50:01 +0530

    Clone

---
+++ app/src/main/java/com/solvedunsolved/myemmo2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.solvedunsolved.myemmo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+}
